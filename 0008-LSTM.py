5caaf0954a8735c7191dd65090f0870c0b914603
Arindam Sahoo <arindams2002@gmail.com>
2025-04-10 03:41:22 +0530

    feat(LSTM): pass tokenizer to TextDataset for improved text processing

--- 0008-LSTM.py
+++ 0008-LSTM.py
@@ -101,8 +101,8 @@ for dataset_name, df in context.items():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # DataLoader
-    train_dataset = TextDataset(X_train, y_train, vocab)
-    test_dataset = TextDataset(X_test, y_test, vocab)
+    train_dataset = TextDataset(X_train, y_train, vocab, tokenizer)
+    test_dataset = TextDataset(X_test, y_test, vocab, tokenizer)
 
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
     test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, collate_fn=collate_fn)
