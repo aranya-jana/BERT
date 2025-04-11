5ff3a5498ebf1fc4cd0ad1a363f9f01416fb9f65
Arindam Sahoo <arindams2002@gmail.com>
2025-04-11 23:15:47 +0530

    feat(workflow): update Python script execution and install dependencies in workflow feat(LoadDatasets): adjust dataset loading paths and add main execution context add: new feature extraction script

--- LoadDatasets.py
+++ LoadDatasets.py
@@ -4,11 +4,14 @@ import os
 def load_datasets():
     # Load datasets
     base_dir = os.path.dirname(__file__)
-    cyberbullying_df = pd.read_csv(os.path.join(base_dir,"cleaned_cyberbullying_tweets.csv"))
-    hatespeech_df = pd.read_csv(os.path.join(base_dir,"cleaned_x_hates.csv"))
+    cyberbullying_df = pd.read_csv(os.path.join(base_dir,"cleaned/cleaned_cyberbullying_tweets.csv"))
+    hatespeech_df = pd.read_csv(os.path.join(base_dir,"cleaned/cleaned_x_hates.csv"))
 
     # Return Merged Dataframe
     return {
         "cyberbullying_df": cyberbullying_df,
         "hatespeech_df": hatespeech_df
     }
+
+if __name__ == "__main__":
+    data_context = load_datasets()
