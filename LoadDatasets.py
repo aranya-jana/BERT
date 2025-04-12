dfa16e4e2ac6ca0a04886ff355de2e02a0ecaee5
Aranya Jana <128471808+aranya-jana@users.noreply.github.com>
2025-04-12 22:05:57 +0530

    Update LoadDatasets.py

--- LoadDatasets.py
+++ LoadDatasets.py
@@ -4,11 +4,11 @@ import os
 def load_datasets():
     # Load datasets
     base_dir = os.path.dirname(__file__)
-    cyberbullying_df = pd.read_csv(os.path.join(base_dir,"cleaned_cyberbullying_tweets.csv"))
+    cyberbullying_df = pd.read_csv(os.path.join(base_dir,"cleaned/cleaned_cyberbullying_tweets.csv"))
     hatespeech_df = pd.read_csv(os.path.join(base_dir,"cleaned_x_hates.csv"))
 
     # Return Merged Dataframe
     return {
         "cyberbullying_df": cyberbullying_df,
-        "hatespeech_df": hatespeech_df
+        # "hatespeech_df": hatespeech_df
     }
